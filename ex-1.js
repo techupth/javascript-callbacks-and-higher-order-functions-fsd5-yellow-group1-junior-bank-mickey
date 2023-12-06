18ceaeb58835fa264ea476da63cf0e4698effb37
junior <jr.foto@hotmail.com>
2023-12-06 16:32:27 +0700

    junior-ex-1-2

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,16 @@
 // Exercise #1: For Each Function
-
-function forEach(array, operation) {
-  // Start coding here
-}
-
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
-// Using `forEach` function here
+function forEach(array, operation) {
+  for (let i = 0; i < array.length; i++) {
+    newEmployeeSalaries.push(operation(array[i]));
+  }
+}
 
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+function newsalary(array) {
+  let add5000 = array + 5000;
+  return add5000;
+}
+forEach(employeeSalaries, newsalary);
+console.log(newEmployeeSalaries);
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,32 @@
 //Exercise #2: At Least Five Function
-
-function atLeastFive(array, operation) {
-  // Start coding here
-}
-
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
+function atLeastFive(array, morethan70) {
+  scoremorethat70 = [];
+  for (let i = 0; i < array.length; i++) {
+    morethan70(array[i]);
+  }
+  if (scoremorethat70.length >= 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function mushmorethan70(number) {
+  if (number >= 70) {
+    scoremorethat70.push(number);
+  }
+}
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoremorethat70 = [];
+
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, mushmorethan70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, mushmorethan70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, mushmorethan70);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
+console.log(scoremorethat70);
